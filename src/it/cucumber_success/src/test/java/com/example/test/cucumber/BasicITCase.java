4c1d3158804ed0191c92724124fb05afac86c43e
Rex Hoffman <rex@e-hoffman.org>
2012-07-15 17:27:51 -0700

    added integration tests....  they get run durring the build and verify success or failure.   Also shows a mismatch between cucumber jvm output json and json the formatter expects.  Test isn't set up to fail the build in this case (yet)

---
+++ src/it/cucumber_success/src/test/java/com/example/test/cucumber/BasicITCase.java
@@ -0,0 +1,12 @@
+package com.example.test.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+import cucumber.junit.Cucumber.Options;
+
+@RunWith(Cucumber.class)
+@Options(dryRun=false, features={"classpath:"}, glue="com.example.test.cucumber",format={"json:target/cucumber.json","junit:target/surefire-reports/report.xml"}, monochrome=true)
+public class BasicITCase {
+
+}
