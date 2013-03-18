79c049f27cbb0db435e3a364b55f0ca0d1c1f013
Kingsley Hendrickse <kingsleyhendrickse@me.com>
2013-03-18 13:41:49 +0000

    updated mojo

--- src/main/java/net/masterthought/cucumber/CucumberReportGeneratorMojo.java
+++ src/main/java/net/masterthought/cucumber/CucumberReportGeneratorMojo.java
@@ -9,53 +9,57 @@ import java.util.List;
 
 /**
  * Goal which generates a Cucumber Report.
+ *
  * @goal generate
  * @phase verify
  */
 public class CucumberReportGeneratorMojo extends AbstractMojo {
 
-   /**
-    * Name of the project.
-    * @parameter expression="${project.name}"
-    * @required
-    */
-   private String projectName;
+    /**
+     * Name of the project.
+     *
+     * @parameter expression="${project.name}"
+     * @required
+     */
+    private String projectName;
 
-   /**
-    * Location of the file.
-    * @parameter expression="${project.build.directory}/cucumber-reports"
-    * @required
-    */
-   private File outputDirectory;
+    /**
+     * Location of the file.
+     *
+     * @parameter expression="${project.build.directory}/cucumber-reports"
+     * @required
+     */
+    private File outputDirectory;
 
-   /**
-    * Location of the file.
-    * @parameter expression="${project.build.directory}/cucumber.json"
-    * @required
-    */
-   private File cucumberOutput;
+    /**
+     * Location of the file.
+     *
+     * @parameter expression="${project.build.directory}/cucumber.json"
+     * @required
+     */
+    private File cucumberOutput;
 
-   public void execute() throws MojoExecutionException {
-      if (!outputDirectory.exists()) {
-         outputDirectory.mkdirs();
-      }
+    public void execute() throws MojoExecutionException {
+        if (!outputDirectory.exists()) {
+            outputDirectory.mkdirs();
+        }
 
-      List<String> list = new ArrayList<String>();
-      list.add(cucumberOutput.getAbsolutePath());
-       ReportBuilder reportBuilder;
+        List<String> list = new ArrayList<String>();
+        list.add(cucumberOutput.getAbsolutePath());
+        ReportBuilder reportBuilder;
 
-       try {
-           System.out.println("About to generate");
-           reportBuilder = new ReportBuilder(list, outputDirectory, "", "1", projectName, false, false, true, false);
-           reportBuilder.generateReports();
-           
-           boolean buildResult = reportBuilder.getBuildStatus();
-           if(!buildResult){
-	         throw new MojoExecutionException("BUILD FAILED - Check Report For Details");
-           }
-            
-      } catch (Exception e) {
-         throw new MojoExecutionException("Error Found:", e);
-      }
-   }
+        try {
+            System.out.println("About to generate");
+            reportBuilder = new ReportBuilder(list, outputDirectory, "", "1", projectName, false, false, true, false, false, "");
+            reportBuilder.generateReports();
+
+            boolean buildResult = reportBuilder.getBuildStatus();
+            if (!buildResult) {
+                throw new MojoExecutionException("BUILD FAILED - Check Report For Details");
+            }
+
+        } catch (Exception e) {
+            throw new MojoExecutionException("Error Found:", e);
+        }
+    }
 }
