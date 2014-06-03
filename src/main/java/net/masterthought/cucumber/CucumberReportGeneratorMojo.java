2c410dc38d28527948b1733d1c6f8df2387e5319
Samuel Cox <sacox@paypal.com>
2014-06-03 07:43:57 -0500

    Support traversing a directory for finding .json files.  The assumption is that all of these represent cucumber reports.  Full credit goes to Petmar from this thread (https://github.com/masterthought/maven-cucumber-reporting-mojo/issues/4).

--- src/main/java/net/masterthought/cucumber/CucumberReportGeneratorMojo.java
+++ src/main/java/net/masterthought/cucumber/CucumberReportGeneratorMojo.java
@@ -1,10 +1,13 @@
 package net.masterthought.cucumber;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -69,12 +72,14 @@ public class CucumberReportGeneratorMojo extends AbstractMojo {
         }
 
         List<String> list = new ArrayList<String>();
-        list.add(cucumberOutput.getAbsolutePath());
-        ReportBuilder reportBuilder;
+		for (File jsonFile : cucumberFiles(cucumberOutput)) {
+			list.add(jsonFile.getAbsolutePath());
+		}
 
         try {
-            System.out.println("About to generate");
-            reportBuilder = new ReportBuilder(list, outputDirectory, "", "1", projectName, skippedFails, undefinedFails, enableFlashCharts, false, false, "", false);
+            getLog().info("About to generate Cucumber report.");
+            ReportBuilder reportBuilder = new ReportBuilder(list, outputDirectory, "", "1", projectName,
+					skippedFails, undefinedFails, enableFlashCharts, false, false, "", false);
             reportBuilder.generateReports();
 
             boolean buildResult = reportBuilder.getBuildStatus();
@@ -86,4 +91,17 @@ public class CucumberReportGeneratorMojo extends AbstractMojo {
             throw new MojoExecutionException("Error Found:", e);
         }
     }
+
+	// Normally, I'd keep this private and use mocks for testing the public contract.
+	// I'm not sure that the author wants to get that serious with this..
+	static Collection<File> cucumberFiles(File file) throws MojoExecutionException {
+		// I'm not entirely convinced we should break the build in this case.
+		if (!file.exists()) {
+			throw new MojoExecutionException(file.getAbsolutePath() + " does not exist.");
+		}
+		if (file.isFile()) {
+			return Arrays.asList(file);
+		}
+		return FileUtils.listFiles(file, new String[] {"json"}, true);
+	}
 }
