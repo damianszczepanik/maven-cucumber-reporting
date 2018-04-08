e3361df6c6bae6b5bfa76d87d43fd08e3849bb02
Bhréin Brannick <bhreinb@gmail.com>
2018-04-08 14:53:43 +0100

    Classifications via properties files

--- src/test/java/net/masterthought/cucumber/CucumberReportGeneratorMojoTest.java
+++ src/test/java/net/masterthought/cucumber/CucumberReportGeneratorMojoTest.java
@@ -1,43 +1,148 @@
 package net.masterthought.cucumber;
 
 import org.junit.Test;
-
 import java.io.File;
 import java.net.URISyntaxException;
-import java.util.Collection;
-
-import static net.masterthought.cucumber.CucumberReportGeneratorMojo.cucumberFiles;
+import java.util.List;
+import static net.masterthought.cucumber.CucumberReportGeneratorMojo.genericFindFiles;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class CucumberReportGeneratorMojoTest {
 
-	@Test
-	public void testCucumberFiles() throws Exception {
-		Collection<File> files = cucumberFiles(testResource("cucumber"));
-		assertThat(files, hasSize(2));
-		assertThat(files, containsInAnyOrder(
-				testResource("cucumber/cuc.json"),
-				testResource("cucumber/subdir/cuc.json")));
+    @Test
+    public void testJsonFilesRegexPattern() throws Exception {
 
-		files = cucumberFiles(testResource("cuc.json"));
-		assertThat(files, hasSize(1));
-		assertThat(files, contains(testResource("cuc.json")));
-	}
+        // Given
+        File targetDirectory = testResource("json");
+        String[] jsonFiles = new String[]{"**/*.json"};
 
-	@Test
-	public void testNotExistingCucumberFile() throws Exception {
-		//given a not existing resource
-		File notExistingFile = new File(testResource("cucumber"), "a/not/existing/path");
+        // When
+        List<String> files = genericFindFiles(targetDirectory,jsonFiles);
 
-		//when I get the list of report files files
-		Collection<File> files = cucumberFiles(notExistingFile);
+        // Then
+        assertThat(files, hasSize(2));
+        assertThat(files, containsInAnyOrder(
+                testResource("json/cuc.json").getAbsolutePath(),
+                testResource("json/subdir/cuc.json").getAbsolutePath()));
 
-		//then the list should be empty
-		assertThat(files, hasSize(0));
-	}
+    }
 
-	private File testResource(String relPath) throws URISyntaxException {
-		return new File(getClass().getResource("/" + relPath).toURI());
-	}
+    @Test
+    public void testJsonFilesNamePattern() throws Exception {
+
+        // Given
+        File targetDirectory = testResource("json");
+        String[] jsonFiles = new String[]{"cuc.json"};
+
+        // When
+        List<String> files = genericFindFiles(targetDirectory,jsonFiles);
+
+        // Then
+        assertThat(files, hasSize(1));
+        assertThat(files, contains(testResource("json/cuc.json").getAbsolutePath()));
+
+    }
+
+    @Test
+    public void testJsonFilesNameAndRegexPattern() throws Exception {
+
+        // Given
+        File targetDirectory = testResource("json");
+        String[] jsonFiles = new String[]{"*/cuc.json"};
+
+        // When
+        List<String> files = genericFindFiles(targetDirectory,jsonFiles);
+
+        // Then
+        assertThat(files, hasSize(1));
+        assertThat(files, contains(testResource("json/subdir/cuc.json").getAbsolutePath()));
+
+    }
+
+    @Test
+    public void testJsonFilesInvalidPath() throws Exception {
+
+        // Given
+        File targetDirectory = testResource("json");
+        String[] jsonFiles = new String[]{"a/not/existing/path"};
+
+        // When
+        List<String> files = genericFindFiles(targetDirectory,jsonFiles);
+
+        // Then
+        assertThat(files, hasSize(0));
+
+    }
+
+    @Test
+    public void testClassificationFilesRegexPattern() throws Exception {
+
+        // Given
+        File targetDirectory = testResource("classification");
+        String[] propertyFiles = new String[]{"**/*.properties"};
+
+        // When
+        List<String> files = genericFindFiles(targetDirectory,propertyFiles);
+
+        // Then
+        assertThat(files, hasSize(2));
+        assertThat(files, containsInAnyOrder(
+                testResource("classification/classifications.properties").getAbsolutePath(),
+                testResource("classification/subdir/classifications.properties").getAbsolutePath()));
+
+    }
+
+    @Test
+    public void testClassificationFilesNamePattern() throws Exception {
+
+        // Given
+        File targetDirectory = testResource("classification");
+        String[] propertyFiles = new String[]{"classifications.properties"};
+
+        // When
+        List<String> files = genericFindFiles(targetDirectory,propertyFiles);
+
+        // Then
+        assertThat(files, hasSize(1));
+        assertThat(files, contains(testResource("classification/classifications.properties").getAbsolutePath()));
+
+    }
+
+    @Test
+    public void testClassificationFilesNameAndRegexPattern() throws Exception {
+
+        // Given
+        File targetDirectory = testResource("classification");
+        String[] propertyFiles = new String[]{"*/classifications.properties"};
+
+        // When
+        List<String> files = genericFindFiles(targetDirectory,propertyFiles);
+
+        // Then
+        assertThat(files, hasSize(1));
+        assertThat(files, contains(testResource("classification/subdir/classifications.properties").getAbsolutePath()));
+
+    }
+
+    @Test
+    public void testClassificationFilesInvalidPath() throws Exception {
+
+        // Given
+        File targetDirectory = testResource("classification");
+        String[] jsonFiles = new String[]{"a/not/existing/path"};
+
+        // When
+        List<String> files = genericFindFiles(targetDirectory,jsonFiles);
+
+        // Then
+        assertThat(files, hasSize(0));
+
+    }
+
+    private File testResource(String relPath) throws URISyntaxException {
+
+        return new File(getClass().getResource("/" + relPath).toURI());
+
+    }
 }
